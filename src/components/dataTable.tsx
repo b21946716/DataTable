3434d8523425699cb23c76c84a77643eaab5807e
Mustafa Emir Yılmaz <b21946716@cs.hacettepe.edu.tr>
2023-09-22 21:11:25 +0300

    updated version of data table without mui

--- src/components/dataTable.tsx
+++ src/components/dataTable.tsx
@@ -1,40 +1,79 @@
-import { DataGrid, GridColDef} from '@mui/x-data-grid';
-import {useState} from "react";
-
-const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID'},
-    { field: 'brand', headerName: 'Brands'},
-];
+import React, { useState } from "react";
 
 const rows = [
-    { id: 1, brand: 'Nike'},
-    { id: 2, brand: 'Lacoste'},
-    { id: 3, brand: 'Koton'},
-    { id: 4, brand: 'Gucci'},
-    { id: 5, brand: 'Adidas'},
-    { id: 6, brand: 'LC Waikiki'},
-    { id: 7, brand: 'H&M'},
-    { id: 8, brand: 'Zara'},
-    { id: 9, brand: 'Mavi'},
-    { id: 10, brand: 'Puma'},
+    { id: 1, brand: 'Nike' },
+    { id: 2, brand: 'Lacoste' },
+    { id: 3, brand: 'Koton' },
+    { id: 4, brand: 'Gucci' },
+    { id: 5, brand: 'Adidas' },
+    { id: 6, brand: 'LC Waikiki' },
+    { id: 7, brand: 'H&M' },
+    { id: 8, brand: 'Zara' },
+    { id: 9, brand: 'Mavi' },
+    { id: 10, brand: 'Puma' },
 ];
 
 export default function DataTable() {
     const [selectedRowIds, setSelectedRowIds] = useState<number[]>([]);
 
-    const handleSelectionModelChange = (selectionModel: any) => {
-        setSelectedRowIds(selectionModel);
+    const handleRowCheckboxChange = (id: number) => {
+        if (selectedRowIds.includes(id)) {
+            setSelectedRowIds(selectedRowIds.filter((rowId) => rowId !== id));
+        } else {
+            setSelectedRowIds([...selectedRowIds, id]);
+        }
+    };
+
+    const isRowSelected = (id: number) => selectedRowIds.includes(id);
+
+    const handleSelectAll = () => {
+        const allRowIds = rows.map((row) => row.id);
+        setSelectedRowIds(allRowIds);
+    };
+
+    const handleUnselectAll = () => {
+        setSelectedRowIds([]);
     };
 
     return (
         <div>
             Selected Row IDs: {selectedRowIds.join(', ')}
-            <DataGrid
-                rows={rows}
-                columns={columns}
-                checkboxSelection
-                onRowSelectionModelChange={handleSelectionModelChange}
-            />
+            <table>
+                <thead>
+                <tr>
+                    <th>
+                        <input
+                            type="checkbox"
+                            onChange={() => {
+                                if (selectedRowIds.length === rows.length) {
+                                    handleUnselectAll();
+                                } else {
+                                    handleSelectAll();
+                                }
+                            }}
+                            checked={selectedRowIds.length === rows.length}
+                        />
+                    </th>
+                    <th>ID</th>
+                    <th>Brands</th>
+                </tr>
+                </thead>
+                <tbody>
+                {rows.map((row) => (
+                    <tr key={row.id}>
+                        <td>
+                            <input
+                                type="checkbox"
+                                checked={isRowSelected(row.id)}
+                                onChange={() => handleRowCheckboxChange(row.id)}
+                            />
+                        </td>
+                        <td>{row.id}</td>
+                        <td>{row.brand}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </div>
     );
 }
